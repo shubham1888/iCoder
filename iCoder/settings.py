1380598a19fa4c70961eae54139e9a8ed7ba6405
Shubham shah <shahshubham1888@gmail.com>
2022-03-08 13:10:09 +0545

    awesome changes

--- iCoder/settings.py
+++ iCoder/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import django_heroku
 import os
+from django.contrib.messages import constants as messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -39,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home.apps.HomeConfig', 
+    'blog.apps.BlogConfig',
 ]
 
 MIDDLEWARE = [
@@ -123,6 +126,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MESSAGE_TAGS = {
+    messages.ERROR:'danger',
+    messages.SUCCESS:'success'
+}
+
 
 # code for heroku -->
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
