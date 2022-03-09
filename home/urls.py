027b2e5d259de5d159dd98cd1058c5c10a6beebf
Shubham shah <shahshubham1888@gmail.com>
2022-03-09 08:06:45 +0545

    login and logout added

--- home/urls.py
+++ home/urls.py
@@ -4,6 +4,7 @@ from home import views
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('blog/', include('blog.urls'),name='blog'),
     path('contact/', views.contact, name="contact"),
     path('about/', views.about, name="about"),
     path('search/', views.search, name="search"),
